cf818d34d4c47ee78129d2ce2275bdd85829746a
Matt Ashford <mattashford1@gmail.com>
2021-08-16 10:33:40 -0400

    router added

--- service-dashboard/src/App.js
+++ service-dashboard/src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as d3 from "d3";
+import Button from "@material-ui/core/Button";
+import {
+  makeStyles,
+  ThemeProvider,
+  createTheme,
+} from "@material-ui/core/styles";
+
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+
+import { orange } from "@material-ui/core/colors";
+
+const useStyles = makeStyles({
+  root: {
+    background: "green",
+    borderRadius: "20px",
+  },
+});
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>im a styled butgggg</Button>;
+}
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: orange[700],
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          {/* <ButtonStyled />
+
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => console.log("clicked")}
+          >
+            Im a button
+          </Button> */}
+          <Switch>
+            <Route exact path="/">
+              {/* <Home /> */}
+            </Route>
+            <Route exact path="/first-class">
+              {/* <FirstClass/> */}
+            </Route>
+            <Route exact path="/marketing-mail">
+              {/* <FirstClass/> */}
+            </Route>
+            <Route exact path="/periodicals">
+              {/* <FirstClass/> */}
+            </Route>
+            <Route exact path="/package-services">
+              {/* <FirstClass/> */}
+            </Route>
+            <Route exact path="/special-services">
+              {/* <FirstClass/> */}
+            </Route>
+          </Switch>
+        </div>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
